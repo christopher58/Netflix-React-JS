e679bbf432b1ffa863b439993afa2793c5f10fa2
christopher58 <christopheratv@hotmail.com>
2022-08-18 11:32:01 +0200

    utviklet til 60 min - slide fungerer men ikke smooth

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -14,9 +14,16 @@ const Row = ({title, fetchURL}) => {
         axios.get(fetchURL).then((response) => {
             setMovies(response.data.results)
         })
-    },[fetchURL])
+    },[fetchURL]);
 
-    console.log(movies)
+    const slideLeft = () => {
+        var slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft - 500;
+      };
+      const slideRight = () => {
+        var slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft + 500;
+      };
     
     
 
@@ -24,14 +31,18 @@ const Row = ({title, fetchURL}) => {
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
       <div className='relative flex items-center group'>
-        <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+        <MdChevronLeft 
+        onClick={slideLeft}
+        className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
           size={40}/>
         <div id={'slider'} className='2-full h-full overflow-x-scroll whitespace-nowrap scrollbar-hide relative'>
         {movies.map((item, id) => (
             <Movie key={id} item={item}/>
         ))}
         </div>
-            <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+            <MdChevronRight
+            onClick={slideRight}
+            className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
           size={40}/>
       </div>
     </>
